20fb3db59fecc04527735a7ff350f34d34325368
Romain Fromi <romain.fromi@gmail.com>
2025-06-10 16:57:55 +0200

    Game Board

--- app/src/material/GameBoardDescription.ts
+++ app/src/material/GameBoardDescription.ts
@@ -3,8 +3,8 @@ import { LocationType } from '@gamepark/rival-cities/material/LocationType'
 import GameBoard from '../images/GameBoard.jpg'
 
 export class GameBoardDescription extends BoardDescription {
-  width = 38.56
-  height = 38.48
+  width = 38
+  height = 38
 
   staticItem = {
     location: {
