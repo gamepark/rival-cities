011f8e59874a6467ff33a4749d113f8da4f5d891
sylvestredavid <david.sylvestre.24@gmail.com>
2025-07-11 08:16:13 +0200

    linear gradient sur player panel

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -1,8 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { City } from '@gamepark/rival-cities/City'
-import Altona from '../images/panels/altona.png'
-import Hamburg from '../images/panels/hambourg.png'
 import { StyledPlayerPanel, usePlayers } from '@gamepark/react-game'
 import { createPortal } from 'react-dom'
 
@@ -16,16 +14,11 @@ export const PlayerPanels = () => {
   return createPortal(
     <>
       {players.map((player) => (
-        <StyledPlayerPanel key={player.id} backgroundImage={backgroundImage[player.id]} player={player} css={panelPosition(player.id)} activeRing />
+        <StyledPlayerPanel key={player.id} player={player} css={panelPosition(player.id)} activeRing />
       ))}
     </>,
     root
   )
-}
-
-const backgroundImage: Record<City, string> = {
-  [City.Altona]: Altona,
-  [City.Hamburg]: Hamburg
 }
 
 const panelPosition = (player: number) => {
@@ -35,6 +28,7 @@ const panelPosition = (player: number) => {
       left: 1em;
       top: 8.5em;
       width: 28em;
+      background: linear-gradient(135deg, white 0%, #3b4e6e 80%);
     `
   }
   return css`
@@ -42,5 +36,6 @@ const panelPosition = (player: number) => {
     right: 1em;
     top: 8.5em;
     width: 28em;
+    background: linear-gradient(135deg, white 0%, #8d433a 80%);
   `
 }
