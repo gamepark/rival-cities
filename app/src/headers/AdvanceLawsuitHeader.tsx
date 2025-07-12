2a0915b93fd105345e4d23081d3618ca5484f95e
sylvestredavid <david.sylvestre.24@gmail.com>
2025-07-12 21:03:02 +0200

    modif header procès

--- app/src/headers/AdvanceLawsuitHeader.tsx
+++ app/src/headers/AdvanceLawsuitHeader.tsx
@@ -1,8 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
 import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { AdvanceLawsuitAction } from '@gamepark/rival-cities/material/Actions/Actions'
 import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { CustomMoveType } from '@gamepark/rival-cities/rules/CustomMoveType'
+import { MemoryType } from '@gamepark/rival-cities/rules/MemoryType'
 import { isCustomMoveType } from '@gamepark/rules-api'
 import { Trans } from 'react-i18next'
 
@@ -14,7 +16,19 @@ export const AdvanceLawsuitHeader = () => {
   const name = usePlayerName(activePlayer)
   const pass = useLegalMove((move) => isCustomMoveType(CustomMoveType.Pass)(move))
 
+  const currentAction = rules.remind(MemoryType.Actions)[0] as AdvanceLawsuitAction
+
   if (itsMe) {
+    if(currentAction?.nbTimeAlreadyAdvanced && currentAction.nbTimeAlreadyAdvanced > 0) {
+      return (
+        <Trans
+          defaults={`header.advance.again.lawsuit.you`}
+          components={{
+            pass: <PlayMoveButton move={pass} />
+          }}
+        />
+      )
+    }
     return (
       <Trans
         defaults={`header.advance.lawsuit.you`}
