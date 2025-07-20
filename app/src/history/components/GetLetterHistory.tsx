644fdde2d3ac186bc9eebb53c4f31e82887b58b1
sylvestredavid <david.sylvestre.24@gmail.com>
2025-07-20 12:24:10 +0200

    fix historique lettre

--- app/src/history/components/GetLetterHistory.tsx
+++ app/src/history/components/GetLetterHistory.tsx
@@ -4,9 +4,9 @@ import { MoveComponentProps, usePlayerName } from '@gamepark/react-game'
 import { Trans } from 'react-i18next'
 
 export const GetLetterHistory = (props: MoveComponentProps) => {
-  const { context } = props
+  const { context, move } = props
   const actionPlayer = context.action.playerId
   const name = usePlayerName(actionPlayer)
 
-  return <Trans defaults="history.get.letter" values={{ player: name }} />
+  return <Trans defaults="history.get.letter" values={{ player: name, quantity: move.quantity }} />
 }
