053f17aa55c6bbdd9e7fd617bdd3850ff5d30965
Romain Fromi <romain.fromi@gmail.com>
2025-08-11 09:48:25 +0200

    Improve PayInkJarMovementCostHeader

--- app/src/headers/PayInkJarMovementCostHeader.tsx
+++ app/src/headers/PayInkJarMovementCostHeader.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
-import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { Picture, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { Memory } from '@gamepark/rival-cities/rules/Memory'
 import { Trans } from 'react-i18next'
+import { getProductIcon, iconCss } from './HeaderIconsCss'
 
 export const PayInkJarMovementCostHeader = () => {
   const me = usePlayerId()
@@ -13,8 +14,24 @@ export const PayInkJarMovementCostHeader = () => {
   const count = rules.remind<number>(Memory.Count)
 
   if (activePlayer === me) {
-    return <Trans defaults="header.pay.product.you" values={{ count }} />
+    return (
+      <Trans
+        defaults="header.ink-jar.cost.you"
+        values={{ count }}
+        components={{
+          product: <Picture src={getProductIcon()} css={iconCss} />
+        }}
+      />
+    )
   }
 
-  return <Trans defaults="header.pay.product.player" values={{ player, count }} />
+  return (
+    <Trans
+      defaults="header.ink-jar.cost.player"
+      values={{ player, count }}
+      components={{
+        product: <Picture src={getProductIcon()} css={iconCss} />
+      }}
+    />
+  )
 }
