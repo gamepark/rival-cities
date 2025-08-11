ada76a788d520981dbaac537e7528fa245268577
Romain Fromi <romain.fromi@gmail.com>
2025-08-11 16:58:36 +0200

    Simplify DonationHeader

--- app/src/headers/DonationHeader.tsx
+++ app/src/headers/DonationHeader.tsx
@@ -1,70 +1,32 @@
 /** @jsxImportSource @emotion/react */
-
-import { Picture, PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { HeaderText, Picture, useRules } from '@gamepark/react-game'
 import { MaterialType } from '@gamepark/rival-cities/material/MaterialType'
 import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { DonationRule } from '@gamepark/rival-cities/rules/actions/DonationRule'
 import { CustomMoveType } from '@gamepark/rival-cities/rules/CustomMoveType'
 import { Memory } from '@gamepark/rival-cities/rules/Memory'
 import { isCustomMoveType, isMoveItemType } from '@gamepark/rules-api'
-import { Trans } from 'react-i18next'
 import Star from '../images/icons/Star.png'
 import { getProductIcon } from './HeaderIconsCss'
 
 export const DonationHeader = () => {
-  const me = usePlayerId()
   const rules = useRules<RivalCitiesRules>()!
-  const activePlayer = rules.getActivePlayer()
-  const player = usePlayerName(activePlayer)
-  const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
-  const donate = useLegalMove(isMoveItemType(MaterialType.StarToken))
   const { cost, product, stars, times } = new DonationRule(rules.game).action
   const count = rules.remind<number>(Memory.Count)
-
-  if (activePlayer === me) {
-    if (count) {
-      return (
-        <Trans
-          defaults="header.donation.pay.you"
-          values={{ count }}
-          components={{
-            product: <Picture src={getProductIcon()} />
-          }}
-        />
-      )
-    } else {
-      return (
-        <Trans
-          defaults="header.donation.you"
-          values={{ cost, stars, times }}
-          components={{
-            donate: <PlayMoveButton move={donate} />,
-            item: <Picture src={getProductIcon(product)} />,
-            star: <Picture src={Star} />,
-            pass: <PlayMoveButton move={pass} />
-          }}
-        />
-      )
-    }
-  }
   if (count) {
-    return (
-      <Trans
-        defaults="header.donation.pay.player"
-        values={{ player, count }}
-        components={{
-          product: <Picture src={getProductIcon()} />
-        }}
-      />
-    )
+    return <HeaderText code="donation.pay" values={{ count }} components={{ product: <Picture src={getProductIcon()} /> }} />
   } else {
     return (
-      <Trans
-        defaults="header.donation.player"
-        values={{ player, cost, stars, times }}
+      <HeaderText
+        code="donation"
+        values={{ cost, stars, times }}
         components={{
           item: <Picture src={getProductIcon(product)} />,
           star: <Picture src={Star} />
+        }}
+        moves={{
+          donate: isMoveItemType(MaterialType.StarToken),
+          pass: isCustomMoveType(CustomMoveType.Pass)
         }}
       />
     )
