26c910f926ae9366383cc025176181e06b60c721
Romain Fromi <romain.fromi@gmail.com>
2025-08-11 09:53:10 +0200

    Improve ChooseActionHeader

--- app/src/headers/ChooseActionHeader.tsx
+++ app/src/headers/ChooseActionHeader.tsx
@@ -5,15 +5,14 @@ import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { Trans } from 'react-i18next'
 
 export const ChooseActionHeader = () => {
-  const player = usePlayerId()
+  const me = usePlayerId()
   const rules = useRules<RivalCitiesRules>()!
-  const activePlayer = rules.game.rule?.player
-  const itsMe = player && activePlayer === player
-  const name = usePlayerName(activePlayer)
+  const activePlayer = rules.getActivePlayer()
+  const player = usePlayerName(activePlayer)
 
-  if (itsMe) {
-    return <Trans defaults="header.choose.action.you" />
+  if (activePlayer === me) {
+    return <Trans defaults="header.choose-action.you" />
   }
 
-  return <Trans defaults="header.choose.action.player" values={{ player: name }} />
+  return <Trans defaults="header.choose-action.player" values={{ player }} />
 }
