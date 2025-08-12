dd567e601719bbb423aed79794ee2d7dab4c9fa3
Romain Fromi <romain.fromi@gmail.com>
2025-08-12 14:45:55 +0200

    Simplify PiracyHeader

--- app/src/headers/PiracyHeader.tsx
+++ app/src/headers/PiracyHeader.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
-
-import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { HeaderText, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { getRival } from '@gamepark/rival-cities/City'
 import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { PiracyRule } from '@gamepark/rival-cities/rules/actions/PiracyRule'
@@ -9,16 +8,12 @@ import { Trans } from 'react-i18next'
 export const PiracyHeader = () => {
   const me = usePlayerId()
   const rules = useRules<RivalCitiesRules>()!
-  const activePlayer = rules.game.rule!.player!
+  const activePlayer = rules.getActivePlayer()!
   const player = usePlayerName(activePlayer)
   const target = usePlayerName(getRival(activePlayer))
   const count = new PiracyRule(rules.game).action.nbProductsToSteal
-
-  if (activePlayer === me) {
-    return <Trans defaults="header.piracy.you" values={{ count, target }} />
-  } else if (me === undefined) {
-    return <Trans defaults="header.piracy.player" values={{ player, count, target }} />
-  } else {
+  if (me && activePlayer !== me) {
     return <Trans defaults="header.piracy.target" values={{ player, count }} />
   }
+  return <HeaderText code="piracy" values={{ count, target }} />
 }
