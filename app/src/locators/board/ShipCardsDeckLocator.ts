19647e33057e06685d4475841d67c1c9f10fc78f
Romain Fromi <romain.fromi@gmail.com>
2025-08-12 17:49:20 +0200

    Improve ShipCardsDeckLocator

--- app/src/locators/board/ShipCardsDeckLocator.ts
+++ app/src/locators/board/ShipCardsDeckLocator.ts
@@ -1,19 +1,11 @@
 import { DeckLocator } from '@gamepark/react-game'
 import { MaterialType } from '@gamepark/rival-cities/material/MaterialType'
-import { Coordinates } from '@gamepark/rules-api'
-import { gameBoardLocator } from '../GameBoardLocator'
 
 class ShipCardsDeckLocator extends DeckLocator {
   parentItemType = MaterialType.GameBoard
-
-  getRotateZ(): number {
-    return 90
-  }
-
-  getCoordinates(): Partial<Coordinates> {
-    const base = gameBoardLocator.coordinates
-    return { x: base.x + 7.5, y: base.y + 4.5 }
-  }
+  gap = { x: 0.05, y: -0.05 }
+  rotateZ = 91
+  positionOnParent = { x: 68, y: 62 }
 }
 
 export const shipCardsDeckLocator = new ShipCardsDeckLocator()
