23b601596d969800e7b73b19ed08a80a56a2c67e
Romain Fromi <romain.fromi@gmail.com>
2025-08-14 17:43:56 +0200

    Improve AllianceCardHelp.tsx

--- app/src/material/help/AllianceCardHelp.tsx
+++ app/src/material/help/AllianceCardHelp.tsx
@@ -1,49 +1,34 @@
 /** @jsxImportSource @emotion/react */
-import { MaterialHelpProps } from '@gamepark/react-game'
-import { Alliance } from '@gamepark/rival-cities/material/Alliance'
-import { FC } from 'react'
+import { MaterialHelpProps, Picture } from '@gamepark/react-game'
+import { Alliance, alliancesData } from '@gamepark/rival-cities/material/Alliance'
+import { CostType } from '@gamepark/rival-cities/material/Cost'
 import { Trans, useTranslation } from 'react-i18next'
+import { getProductIcon } from '../../headers/HeaderIconsCss'
+import Letter from '../../images/icons/Letter.png'
+import Product from '../../images/icons/Product.png'
 import { components, note } from './utils'
 
-export const AllianceCardHelp: FC<MaterialHelpProps> = ({ item }) => {
+export function AllianceCardHelp({ item }: MaterialHelpProps) {
   const { t } = useTranslation()
-
+  const alliance = item.id as Alliance
+  const upkeepCost = alliancesData[alliance].cost
+  const upkeepItem = upkeepCost.type === CostType.Letters ? Letter : upkeepCost.type === CostType.AnyProducts ? Product : getProductIcon(upkeepCost.product)
   return (
     <>
-      <h2>{t('help.alliance.card')}</h2>
+      <h2>{t('help.card.alliance')}</h2>
       <p>
-        <Trans defaults="help.alliance.card.descr" />
+        <Trans defaults="help.card.alliance.text" components={{ ...components, letter: <Picture src={Letter} /> }} />
       </p>
-      <h3>{t(`help.alliance.card.${item.id}`)}</h3>
+      <h3>{t(`help.card.alliance.${item.id}`)}</h3>
       <p>
-        <b>{t('help.price')}</b> <Price cardId={item.id as Alliance} />
+        <Trans defaults="help.card.alliance.upkeep" values={upkeepCost} components={{ ...components, item: <Picture src={upkeepItem} /> }} />
       </p>
       <p>
-        <Trans defaults={`help.alliance.card.${item.id}.descr`} components={components} />
+        <Trans defaults={`help.card.alliance.${item.id}.text`} components={components} />
       </p>
       <p css={note}>
-        <Trans defaults="help.alliance.card.win" components={components} />
+        <Trans defaults="help.card.alliance.win" components={components} />
       </p>
     </>
   )
-}
-
-const Price = ({ cardId }: { cardId: Alliance }) => {
-  switch (cardId) {
-    case Alliance.Amsterdam:
-      return <Trans defaults="help.price.choice" values={{ quantity: 2 }} components={components} />
-    case Alliance.Bruxelles:
-      return <Trans defaults="help.price.furniture" values={{ quantity: 1 }} components={components} />
-    case Alliance.Gdansk:
-      return <Trans defaults="help.price.letter" values={{ quantity: 1 }} components={components} />
-    case Alliance.Kjobenhavn:
-      return <Trans defaults="help.price.beer" values={{ quantity: 2 }} components={components} />
-    case Alliance.LeHavre:
-    case Alliance.London:
-    case Alliance.Novgorod:
-    case Alliance.Oslo:
-      return <Trans defaults="help.price.choice" values={{ quantity: 1 }} components={components} />
-    default:
-      return <></>
-  }
 }
