dd1942628736cfdba43b4793e8f0f9898ad91ebe
Romain Fromi <romain.fromi@gmail.com>
2025-08-14 15:07:34 +0200

    Fix keepMoveSecret

--- rules/src/RivalCitiesRules.ts
+++ rules/src/RivalCitiesRules.ts
@@ -218,7 +218,7 @@ export class RivalCitiesRules
         isCustomMoveType(CustomMoveType.ChooseAlliance)(move) || isMoveItemType(MaterialType.Product)(move) || isMoveItemType(MaterialType.AllianceCard)(move)
       )
     }
-    return []
+    return false
   }
 
   giveTime(): number {
