bf7a4ef0b6b584d1b860f20bfb1a745b4241d4fb
Romain Fromi <romain.fromi@gmail.com>
2025-08-15 15:42:08 +0200

    Improve FactoryHelp.tsx

--- app/src/material/help/FactoryHelp.tsx
+++ app/src/material/help/FactoryHelp.tsx
@@ -1,13 +1,26 @@
 /** @jsxImportSource @emotion/react */
+import { MaterialHelpProps, useRules } from '@gamepark/react-game'
+import { LocationType } from '@gamepark/rival-cities/material/LocationType'
+import { MaterialType } from '@gamepark/rival-cities/material/MaterialType'
+import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { Trans, useTranslation } from 'react-i18next'
 import { components } from './utils'
 
-export function FactoryHelp() {
+export function FactoryHelp({ item }: MaterialHelpProps) {
   const { t } = useTranslation()
-
+  const rules = useRules<RivalCitiesRules>()!
   return (
     <>
       <h2>{t('help.factory')}</h2>
+      {item.location?.type === LocationType.FactorySupply && (
+        <p>
+          <Trans
+            defaults="help.factory.supply"
+            values={{ count: rules.material(MaterialType.Factory).location(LocationType.FactorySupply).getQuantity() }}
+            components={components}
+          />
+        </p>
+      )}
       <p>
         <Trans defaults="help.factory.text" components={components} />
       </p>
