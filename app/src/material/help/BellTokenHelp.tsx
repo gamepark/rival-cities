4c9cbd71c27da6f5b232fdd8f77d36a6afe95632
Romain Fromi <romain.fromi@gmail.com>
2025-08-15 17:14:07 +0200

    BellTokenHelp.tsx

--- app/src/material/help/BellTokenHelp.tsx
+++ app/src/material/help/BellTokenHelp.tsx
@@ -1,15 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import { Trans, useTranslation } from 'react-i18next'
-import { components } from './utils'
+import { components, note } from './utils'
 
 export function BellTokenHelp() {
   const { t } = useTranslation()
 
   return (
     <>
-      <h2>{t('help.bell.token')}</h2>
+      <h2>{t('help.bell')}</h2>
       <p>
-        <Trans defaults="help.bell.token.descr" components={components} />
+        <Trans defaults="help.bell.text" components={components} />
+      </p>
+      <p css={note}>
+        <Trans defaults={'help.bell.tie-breaker'} components={components} />
       </p>
     </>
   )
