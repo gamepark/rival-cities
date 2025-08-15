8b86379fd8a46ea108f4ddb29b100af65e678999
Romain Fromi <romain.fromi@gmail.com>
2025-08-15 15:47:29 +0200

    Improve StarTokenHelp.tsx

--- app/src/material/help/StarTokenHelp.tsx
+++ app/src/material/help/StarTokenHelp.tsx
@@ -1,15 +1,28 @@
 /** @jsxImportSource @emotion/react */
+import { MaterialHelpProps, useRules } from '@gamepark/react-game'
+import { LocationType } from '@gamepark/rival-cities/material/LocationType'
+import { MaterialType } from '@gamepark/rival-cities/material/MaterialType'
+import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
 import { Trans, useTranslation } from 'react-i18next'
 import { components } from './utils'
 
-export function StarTokenHelp() {
+export function StarTokenHelp({ item }: MaterialHelpProps) {
   const { t } = useTranslation()
-
+  const rules = useRules<RivalCitiesRules>()!
   return (
     <>
-      <h2>{t('help.star.token')}</h2>
+      <h2>{t('help.star')}</h2>
+      {item.location?.type === LocationType.StarTokenSupply && (
+        <p>
+          <Trans
+            defaults="help.star.supply"
+            values={{ count: rules.material(MaterialType.StarToken).location(LocationType.StarTokenSupply).getQuantity() }}
+            components={components}
+          />
+        </p>
+      )}
       <p>
-        <Trans defaults="help.star.token.descr" components={components} />
+        <Trans defaults="help.star.text" components={components} />
       </p>
     </>
   )
