ec9902d20368f4dbaf1cd6c53128d58f20d63fa5
Romain Fromi <romain.fromi@gmail.com>
2025-08-16 16:47:46 +0200

    Improve MoveInkJarHistory.tsx

--- app/src/history/components/MoveInkJarHistory.tsx
+++ app/src/history/components/MoveInkJarHistory.tsx
@@ -1,16 +1,31 @@
 /** @jsxImportSource @emotion/react */
-import { MaterialLogProps, Picture, usePlayerName } from '@gamepark/react-game'
+import { MaterialLogProps, Picture, PlayMoveButton, usePlayerName } from '@gamepark/react-game'
+import { specialActionCardPlaces } from '@gamepark/rival-cities/constantes'
+import { LocationType } from '@gamepark/rival-cities/material/LocationType'
 import { MaterialType } from '@gamepark/rival-cities/material/MaterialType'
 import { RivalCitiesRules } from '@gamepark/rival-cities/RivalCitiesRules'
-import { MoveItem } from '@gamepark/rules-api'
+import { MaterialMoveBuilder, MoveItem } from '@gamepark/rules-api'
 import { Trans } from 'react-i18next'
 import InkJar from '../../images/tokens/InkJar.png'
 import { historyIcon } from './historyCss'
+import displayMaterialHelp = MaterialMoveBuilder.displayMaterialHelp
 
 export function MoveInkJarHistory({ move, context }: MaterialLogProps<MoveItem>) {
   const player = usePlayerName(context.action.playerId)
-  const origin = new RivalCitiesRules(context.game).material(MaterialType.InkJar).getItem()!.location.id as number
+  const rules = new RivalCitiesRules(context.game)
+  const origin = rules.material(MaterialType.InkJar).getItem()!.location.id as number
   const destination = move.location.id as number
   const distance = (20 + destination - origin) % 20
-  return <Trans defaults="history.move-ink-jar" values={{ player, distance }} components={{ inkJar: <Picture css={historyIcon} src={InkJar} /> }} />
+  const cardType = specialActionCardPlaces.includes(destination) ? MaterialType.SpecialActionCard : MaterialType.BasicActionCard
+  const card = rules.material(cardType).location(LocationType.ActionCardSpace).locationId(destination).getItem()
+  return (
+    <Trans
+      defaults="history.move-ink-jar"
+      values={{ player, distance }}
+      components={{
+        inkJar: <Picture css={historyIcon} src={InkJar} />,
+        card: <PlayMoveButton move={displayMaterialHelp(MaterialType.SpecialActionCard, card)} transient />
+      }}
+    />
+  )
 }
